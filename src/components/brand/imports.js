3aa8e22701cac56fbcb021b0236c92cb50c4fea6
Pranay ALone git <alonepranay123@gmail.com>
2023-05-30 20:23:45 +0530

    Made some Changes

---
+++ src/components/brand/imports.js
@@ -0,0 +1,6 @@
+import slack from "../../Assests/slack.png";
+import atlassian from "../../Assests/atlassian.png";
+import dropbox from "../../Assests/dropbox.png";
+import shopify from "../../Assests/shopify.png";
+import google from "../../Assests/google.png";
+export { google, slack, atlassian, dropbox, shopify };
