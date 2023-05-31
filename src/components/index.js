e7df99514d41798328eb0dc611b9ba8552881cda
Pranay ALone git <alonepranay123@gmail.com>
2023-05-31 17:56:55 +0530

    Completed The GPT3

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Article } from "./article/Article";
 export { default as Brand } from "./brand/Brand";
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Cta } from "./cta/CTA";
+export { default as Features } from "./feature/Feature";
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,7 @@
+import blog01 from "../../Assests/blog01.png";
+import blog02 from "../../Assests/blog02.png";
+import blog03 from "../../Assests/blog03.png";
+import blog04 from "../../Assests/blog04.png";
+import blog05 from "../../Assests/blog05.png";
+
+export { blog01, blog02, blog03, blog04, blog05 };
